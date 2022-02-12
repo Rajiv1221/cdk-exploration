a225e3752ea4795d0df9e2420302a1d7c210c5f8
Ashutosh <ashutoshcse10@gmail.com>
2022-02-12 14:06:26 +0530

    Initial commit

--- cdk_project/cdk_project_stack.py
+++ cdk_project/cdk_project_stack.py
@@ -1,11 +1,13 @@
 from aws_cdk import (
     # Duration,
-    Stack,
+    core ,
+    aws_s3  as  _s3,
+   
     # aws_sqs as sqs,
 )
 from constructs import Construct
 
-class CdkProjectStack(Stack):
+class CdkProjectStack(core.Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
@@ -17,3 +19,12 @@ class CdkProjectStack(Stack):
         #     self, "CdkProjectQueue",
         #     visibility_timeout=Duration.seconds(300),
         # )
+
+        _s3.Bucket(
+            self,
+            "muBucketID",
+            bucket_name= "bucket-cdk-2004",
+            versioned= True,
+            encryption=_s3.BucketEncryption.KMS_MANAGED
+            
+        )
